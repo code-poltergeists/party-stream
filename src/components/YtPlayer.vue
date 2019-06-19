cf86a5f2447cf7e2ffc502e8315c3e3bc8c7a604
radupasparuga <radupasparuga25@gmail.com>
2019-06-19 21:52:37 +0300

    Added youtube iframe

---
+++ src/components/YtPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <iframe id="ytplayer"
+      type="text/html" 
+      width="720" 
+      height="405" 
+      src="https://www.youtube.com/embed/M7lc1UVf-VE" 
+      frameborder="0" 
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class YtPlayer extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
