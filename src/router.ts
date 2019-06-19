0723e24490ad9303d3a037251d6827808c08ca03
radupasparuga <radupasparuga25@gmail.com>
2019-06-19 21:52:37 +0300

    Added youtube iframe

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
+import YtPlayer from './components/YtPlayer.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'dashboard',
       component: Dashboard,
+    },
+    {
+      path: '/ytplayer',
+      name: 'ytplayer',
+      component: YtPlayer,
     },
   ],
 });
