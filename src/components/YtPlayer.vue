dba7c69f45a477a1d3e8e2651a95cc487498750b
radupasparuga <radupasparuga25@gmail.com>
2019-06-20 16:59:55 +0300

    Added vue-youtube integration

--- src/components/YtPlayer.vue
+++ src/components/YtPlayer.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <iframe id="ytplayer"
-      type="text/html" 
-      width="720" 
-      height="405" 
-      src="https://www.youtube.com/embed/M7lc1UVf-VE" 
-      frameborder="0" 
-      allowfullscreen
-    />
+    <youtube video-id="BBJa32lCaaY" />
   </div>
 </template>
 
