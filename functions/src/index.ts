9c4a5a87d3d8808d0c2055282a2d4ab8a84140cf
radupasparuga <radupasparuga25@gmail.com>
2019-06-23 15:19:24 +0300

    Added body-parser

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,9 @@ import * as express from 'express';
 
 const app = express();
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
 app.get('/', (req, res) => {
   res.send(`
   <!doctype html>
@@ -17,6 +20,16 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.send("hi");
+});
+
+app.post('/startPlayer', (req, res) => {
+  let data = req.body;
+  res.json(data);
+});
+
+app.post('/pausePlayer', (req, res) => {
+  let data = req.body;
+  res.json(data);
 });
 
 exports.app = functions.https.onRequest(app);
