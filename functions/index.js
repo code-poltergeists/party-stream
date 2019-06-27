469a305a78e3170938b8fee930494857e5b52088
Radu Pasparuga <radupasparuga25@gmail.com>
2019-06-27 21:37:30 +0300

    Added index.js

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions")
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/test', function(req, res){
+	res.send('<h1>Hi! I\'m the socket.io frontend page don\'t look at me!');
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+exports.app = functions.https.onRequest(app);
+
